0cf5ee6631968dc4ae8e80f71cd331e91f706a5f
Dmytro Lukianov <dimalukianovweb@gmail.com>
2024-07-10 12:09:36 +0300

    add history page

--- src/app/admin/products/_components/products-table.tsx
+++ src/app/admin/products/_components/products-table.tsx
@@ -112,12 +112,6 @@ export const ProductsTable = async () => {
           </TableRow>
         ))}
       </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
     </Table>
   );
 };
